6939bd5f8f2d19909c163b2549ce8ea885ea715f
Matthew Wilkins, PhD <mrwilkins06@gmail.com>
2021-07-05 21:00:48 -0500

    add dataset
    dsfasdfsa

--- R/enciphR.R
+++ R/enciphR.R
@@ -1,23 +1,20 @@
-#' enciphR
-#'
-#' encipher any text string using an alphabetic shift
-#'
-#' @param x string to be enciphered
-#' @param alg algorithm for cipher (e.g. 1 to shift all letter forward 1 `[A->B]`; -2 to shift all letters back 2 places `[A->Y]`)
-#' @param key do you want a key to be included in output?; default=F; If key=T, output will be a list, rather than a vector.
-#' @export
+#nonShiny ciphR code for enciphering a string
+# x= string to be encoded
+# alg= algorithm for cipher/code (could be +/1 N for alphabetic shift)
+# default output is converted vector
+# if key=T, output will be list w/ converted text & key as items
 
 enciphR<-function(x,alg,key=F){
   X.vec<-unlist(strsplit(x,fixed=T,split=""))
   x.vec<-tolower(X.vec)#all lower case as vector
-
+     
      #define new alphabet
       alphabet<-1:26+alg
       alphabet.shifted.idx<-sapply(alphabet,function(x) {if(x>26){x-26}else{ if(x<1){x+26}else{x}}})
       alphabet.shifted<-letters[alphabet.shifted.idx]
-
+      
       keyMat=cbind(IN=letters,OUT=alphabet.shifted)
-
+      
     #encipher
       x1.1<-as.vector(sapply(1:length(x.vec),function(s) {
          if(!x.vec[s]%in%letters){x.vec[s]}else{#If nonletter, leave it alone, else...
@@ -34,7 +31,22 @@ enciphR<-function(x,alg,key=F){
       return(out)
 }
 
+enciphR("Hello, my name is Simon3223",alg=+3,key=T)
 
+#ggGraph is a ggplot2 object
+#alg is an algorithm for enciphering strings for the enciphR function (e.g. +2)
+ggCiphR<-function(ggGraph,alg){
+  g<-ggGraph
+  pass2enciphR=function(x){enciphR(x,alg=alg)}
+  #process all labels
+  g$labels<-lapply(g$labels,pass2enciphR)
 
-
+  #process custom legend if present (sometimes works, depending on ggplot object)
+  try(if(length(g$scales$scales)>0){
+    for(i in 1:length(g$scales$scales)){
+      g$scales$scales[[i]]$name=enciphR(g$scales$scales[[i]],alg=alg)
+    }
+  })
+  g
+}
 
