3f8ed5197c2ea997e7183c32a9753609a82c4340
Matt Wilkins <matt@galacticpolymath.com>
2021-09-22 21:36:26 -0500

    big update to showPhylo to be more resilient and especially to improve documentation

--- R/showPhylo.R
+++ R/showPhylo.R
@@ -2,21 +2,24 @@
 #'
 #' A *relatively* simple function for generating presentation-ready phylogenies, given a set of common or scientific names. It works best for a few species, since the goal is to make a phylogeny for pedagogical purposes, so the focus is on details, not quantity.
 #'
-#' You have the ability to look up matching common or scientific names for taxa, plot them in an evolutionary tree based on the Open Tree of Life Project, date taxa using datelife, if data are available, plot a 'phylopic' (i.e. a silhouette of the organism) if available, pull down the profile image from the organisms Wikipedia page, or provide a custom image, and label the pictures with the scientific and common names. The result is a customizable ggplot2 object. This is a convenience wrapper that merges functionality from \pkg{ggplot2}, \pkg{ggtree}, \pkg{datelife}, \pkg{rotl},and \pkg{taxize}, among other packages.
+#' You have the ability to look up matching common or scientific names for taxa, plot them in an evolutionary tree based on the Open Tree of Life Project, date taxa using \code{\link[datelife]{datelife_search}}, if data are available, plot a \href{http://phylopic.org/}{phylopic} (i.e. a silhouette of the organism) if available, pull down the profile image from each organism's Wikipedia page, or provide a custom image, and label the pictures with the scientific and common names. The result is a customizable ggplot2 object. This is a convenience wrapper that merges functionality from \pkg{ggplot2}, \pkg{ggtree}, \pkg{datelife}, \pkg{rotl},and \pkg{taxize}, among other packages.
+#'
+#' The impetus for this package was the need to make a figure showing that the many animals we call "panthers" are actually really distantly related cat species. Our genetic rescue lesson, sponsored by \href{http://swfitz.com/}{Dr. Sarah Fitzpatrick's lab}, uses the example of the Florida panther as a charismatic application of her research (which mostly uses guppies as a model for understanding how gene flow can save small, highly inbred populations from extinction).
 #'
 #' @param speciesNames a name or vector of common or scientific names of organisms of interest (in quotes)
 #' @param nameType what type of name are you supplying? Either "sci" or "common"
 #' @param dateTree try to scale the tree to estimated divergence times? Uses \code{\link[datelife]{datelife_search}} with summary_format="phylo_median." default= T
+#' @param labelType which names to label tree "leaves"? Options= "s" for scientific, "c" for common, and "b" for both (default="b")
 #' @param labelOffset how far from the tree tips do you want to put labels? default=0.3 (in proportion of x-axis units)
 #' @param aspectRatio doesn't actually work yet; the output phylogeny is always square for the moment
 #' @param pic what type of species image do you want to plot? options="wiki" (Wikipedia page profile image), "phylopic" (species' silhouette from the PhyloPic repository), "cust" (custom images: must be named .jpg or .png with names matching speciesNames in the picSaveDir folder), or "none"
-#' @param dotsConnectText do you want a dotted line to go from the text to the labels? default=F
+#' @param dotsConnectText do you want a dotted line to go from the text to the labels? default=FALSE
 #' @param picSize how big to scale images, where 1=100%; .5=50%; default=1
 #' @param picSaveDir location for saving downloaded images; default=fs::path(tempdir(),"showPhylo")
 #' @param optPicWidth picture width in pixels for optimized versions of images saved if using pic="cust"; default= 200
 #' @param picBorderWidth for pic="wiki" or "cust," what size border would you like around your image (as a %); default=10
 #' @param picBorderCol color of image border; default="#363636" (a dark, near-black color)
-#' @param openDir for pic="wiki" or "cust," do you want to open the picSaveDir after processing files? default=F
+#' @param openDir for pic="wiki" or "cust," do you want to open the picSaveDir after processing files? default=FALSE
 #' @param xAxisPad spacing between the phylogeny and the x-axis (scale is 1 is the distance between two sister taxa); default=.2
 #' @param xTitlePad spacing between x-axis title and x numbers; default=20
 #' @param numXlabs the number of year markers on the x-axis; default=8
@@ -26,16 +29,79 @@
 #' @param phyloCol color of the phylogeny lines; default= "#363636"
 #' @param textCol color of the axis and tip labels; default= "#363636"
 #' @param plotMar margins around the plot area in proportional screen width units; note the right margin is much wider to make room for tip labels; default=c(t=.02,r=.1,b=.02,l=.02) for top, right, bottom, left
-#' @param clearCache delete cached images and taxonomic names? Passed to getPhyloNames, getWikiPics, and also applies to optimized custom images; default=F
-#' @param quiet suppress verbose feedback from the taxize package? Passed to getPhyloNames and get WikiPic helper functions. Default=T
+#' @param clearCache delete cached images and taxonomic names? Passed to getPhyloNames, getWikiPics, and also applies to optimized custom images; default=FALSE
+#' @param quiet suppress verbose feedback from the taxize package? Passed to getPhyloNames and get WikiPic helper functions. Default=TRUE
+#' @param ... pass other parameters to \code{\link[ggtree]{ggtree}}
 #' @md
 #' @import ggtree datelife
 #' @export
 #'
-showPhylo<-function(speciesNames,nameType,dateTree=T,labelOffset=.45,aspectRatio=1,pic="wiki",dotsConnectText=F,picSize=1,picSaveDir,optPicWidth=200,picBorderWidth=10,picBorderCol="#363636",openDir=F,xAxisPad=.2,xTitlePad=20,numXlabs=8,textScalar=1,xTitleScalar=1,phyloThickness=1.2,phyloCol="#363636",textCol="#363636",plotMar=c(t=.02,r=.36,b=.02,l=.02),clearCache=F,quiet=T){
+#' @examples
+#' require(ggplot2);require(ggtree)
+#' #declare some species common names
+#' speciesNames <- c("puma","leopard","jaguar","domestic cat")
+#'
+#' #Make a dated phylogeny with them (by default, median divergence times are estimated
+#' #using the datelife package and images are added from Wikipedia)
+#' showPhylo(speciesNames,"c")
+#'
+#' #If a particular combination of organisms lacks divergence time data (particularly extinct species),
+#' #causing an error
+#' \dontrun{
+#' showPhylo(c("potbellied seahorse","leafy seadragon",
+#' "oarfish","Tyrannosaurus"),"common")
+#' }
+#' #you can possibly still produce an undated phylogeny (cladogram) using the open tree of life data
+#' showPhylo(c("potbellied seahorse","leafy seadragon",
+#' "oarfish","Tyrannosaurus"),"c",dateTree=FALSE)
+#'
+#' #Instead of Wikipedia images for each species, we can try to find a phylopic (silhouette)
+#' showPhylo(c("potbellied seahorse","leafy seadragon","oarfish",
+#' "Tyrannosaurus"),"c",dateTree=FALSE,pic="phylopic")
+#'
+#' #In the case above, the butterfly was the wrong image, so we could try searching using the genus,
+#' # (note: change nameType to "scientific");
+#' showPhylo(c("Hippocampus","phycodurus","Regalecus","Tyrannosaurus"),
+#' "scientific",dateTree=FALSE,pic="p")
+#'
+#' #Not great, but \href{http://phylopic.org/}{phylopic} is open source,
+#' #so you can upload your own silhouette
+#'
+#' #If you have a ton of species, you can just say no pics
+#' noms<-c("rock hyrax","Hippopotamus","Eastern gray squirrel","Asian elephant",
+#' "African elephant","groundhog","meerkat","wolverine")
+#' showPhylo(noms,"c",pic="n",dateTree=FALSE)
+#'
+#' #Doesn't look great, so we can connect the tree tips with the text
+#' #and make the tree blue (why not?); Also, let's just show common names
+#' showPhylo(noms,"c",pic="n",dateTree=FALSE,phyloCol="royalblue1",dotsConnectText=TRUE,nameType="c")
+#'
+#' #Now let's shorten the gap between the tree and the text, and increase text size
+#' (g<-showPhylo(noms,"c",pic="n",dateTree=FALSE,phyloCol="royalblue1",dotsConnectText=TRUE,
+#' nameType="c",labelOffset=.2,textScalar=1.2))
+#'
+#' #once we're satisfied with the tree, we can edit it like any other ggplot
+#' (g2<-g+labs(title="Phylogeny showing how weird rock hyraxes are:",
+#' subtitle="They look like groundhogs, but are much closer related to elephants")+
+#' theme(plot.title=element_text(size=18)) )
+#'
+#' #we can also use \href{https://yulab-smu.top/treedata-book/chapter5.html}{ggtree functions}
+#' #(like \code{\link[ggtree]{geom_highlight}} to do all sorts of
+#' #stuff, like highlight our key species (you can figure out which node that is here with g2$data)
+#' g2+geom_highlight(mapping=aes(subset=node==6),fill="salmon")
+#'
+#'
+#' #OK, let's simplify it, add divergence time and get some pics back
+#' g3<-showPhylo(noms[c(1,3,5,6,7)], "c")
+#' g3+geom_highlight(mapping=aes(subset=node==4),fill="salmon")+
+#' labs(title="Phylogeny showing how weird rock hyraxes are:",
+#' subtitle="They look like groundhogs, but are much closer related to elephants")+
+#' theme(plot.title=element_text(size=18))
+#'
+showPhylo<-function(speciesNames,nameType,dateTree=TRUE,labelType="b",labelOffset=.45,aspectRatio=1,pic="wiki",dotsConnectText=FALSE,picSize=1,picSaveDir,optPicWidth=200,picBorderWidth=10,picBorderCol="#363636",openDir=FALSE,xAxisPad=.2,xTitlePad=20,numXlabs=8,textScalar=1,xTitleScalar=1,phyloThickness=1.2,phyloCol="#363636",textCol="#363636",plotMar=c(t=.02,r=.36,b=.02,l=.02),clearCache=FALSE,quiet=TRUE,...){
 
   #Check for extra missing dependencies
-  missingpkgs<-unlist(sapply(c("magick","rvest"),function(pkg){
+  missingpkgs<-unlist(sapply(c("magick"),function(pkg){
    if (!requireNamespace(pkg, quietly = TRUE)) {pkg}else{NULL}
     }))
   if(length(missingpkgs>0)){
@@ -97,7 +163,7 @@ showPhylo<-function(speciesNames,nameType,dateTree=T,labelOffset=.45,aspectRatio
     if(dateTree){
       tryCatch({
         message(rep("-",55),"\n Attempting to scale the tree to divergence times...\n",rep("-",55))
-        message(" Tip: If it takes more than a few seconds, it's probably going to fail.\n")
+        message(" Tip: If it takes more than a few seconds, it's probably going to fail. (You may want to hit stop).\n")
         tree_final<-datelife::datelife_search(tree,summary_format="phylo_median")
 
        },error=function(e) {
@@ -107,11 +173,18 @@ showPhylo<-function(speciesNames,nameType,dateTree=T,labelOffset=.45,aspectRatio
     # This modifies tip.labels destructively ----------------------------------
     #make an index to go between tree tips and tol_taxa object
     tree_final$tip.label.backup<-tree_final$tip.label
-    tipIndx<-match(tree_final$tip.label.backup,gsub(" ","_",tol_taxa$unique_name))
-    sci_tmp<-gsub(" ","~",tol_taxa$unique_name[tipIndx])
+
+
+    # Clean names -------------------------------------------------------------
+    #in case of weird names with parentheticals; e.g. Phyllopteryx (genus in Deuterostomia)
+    tol_names_cleaned<-gsub("(.*) ?[(].*[)](.*)","\\1\\2",tol_taxa$unique_name)
+    tipIndx<-match(tree_final$tip.label.backup,gsub(" ","_",tol_names_cleaned))
+    sci_tmp<-gsub(" ","~",tol_names_cleaned[tipIndx])
     com_tmp<-paste0("(",gsub(" ","~",tol_taxa$common_name[tipIndx]),")")
-    tree_final$tip.label<-paste0("atop(bolditalic(",sci_tmp,"),",
-                                              gsub("([^~()*]*'[^~()*]*)","\"\\1\"",fixed=F,com_tmp)    ,")")
+    tree_final$tip.label<-switch(labelType,b= paste0("atop(bolditalic(",sci_tmp,"),",
+                                              gsub("([^~()*]*'[^~()*]*)","\"\\1\"",fixed=F,com_tmp)    ,")"),
+                                           c= gsub("[()]","",com_tmp),
+                                           s= sci_tmp)
 
 
     # Look up and cache phylopic image UIDs in an efficient manner ------------
@@ -159,7 +232,7 @@ showPhylo<-function(speciesNames,nameType,dateTree=T,labelOffset=.45,aspectRatio
           missingImgs<-which(is.na(wikiPics$img_loc))
           common_names_in_order_of_tips<-gsub("\\(|\\)","",
                                               tol_taxa$common[match(tree_final$tip.label.backup,
-                                                                    gsub(" ","_",tol_taxa$unique_name))])
+                                                                    gsub(" ","_",tol_names_cleaned))])
           #replace search_term with common name
           wikiPics$search_term[missingImgs]<-common_names_in_order_of_tips[missingImgs]
           #search again
@@ -208,7 +281,7 @@ showPhylo<-function(speciesNames,nameType,dateTree=T,labelOffset=.45,aspectRatio
       #check if images exist
       message(rep("-",45),"\n Checking for custom species images in picSaveDir=\n  > ",picSaveDir,"\n",rep("-",50))
       #Reorder tol_taxa to match tree as our Rosetta for matching file names
-      tol_taxa.orderedByTree<-tol_taxa[match(tree_final$tip.label.backup,gsub(" ","_",tol_taxa$unique_name)),]
+      tol_taxa.orderedByTree<-tol_taxa[match(tree_final$tip.label.backup,gsub(" ","_",tol_names_cleaned)),]
       #search_string
       img_files<-list.files(fs::path(picSaveDir),pattern="\\.png|\\.jpeg|\\.jpg")
       img_files.stndzd <- gsub(" |-","_",tolower(img_files))
@@ -291,7 +364,7 @@ showPhylo<-function(speciesNames,nameType,dateTree=T,labelOffset=.45,aspectRatio
                                 panel.border=ggplot2::element_blank())
 
     #Define basic tree plot before modifying in steps
-    g0<-ggtree::ggtree(tree_final,size=phyloThickness,color=phyloCol)+theme_phylo
+    g0<-ggtree::ggtree(tree_final,size=phyloThickness,color=phyloCol,...)+theme_phylo
 
     #Extract some info from base plot
     timescale<-ggplot2::layer_scales(g0)$x$get_limits()[2]
