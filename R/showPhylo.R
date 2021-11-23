248617198ddb6574c604769b7ce83f4d8b292102
Matt Wilkins <matt@galacticpolymath.com>
2021-11-22 21:21:12 -0600

    forgot to save

--- R/showPhylo.R
+++ R/showPhylo.R
@@ -120,7 +120,7 @@
 
 
 # Run the code through helper function to handle suppression of all output (silent mode)
-showPhylo<-function(speciesNames,nameType,dateTree=TRUE,labelType="b",labelOffset=.45,aspectRatio=1,pic="wiki",dotsConnectText=FALSE,picSize=1,picSaveDir,optPicWidth=200,picBorderWidth=10,picBorderCol="#363636",openDir=FALSE,xAxisPad=.2,xTitlePad=20,numXlabs=8,textScalar=1,xTitleScalar=1,phyloThickness=1.2,phyloCol="#363636",textCol="#363636",plotMar=c(t=.02,r=.5,b=.02,l=.02),clearCache=FALSE,quiet=TRUE,silent=FALSE,...){
+showPhylo<-function(speciesNames,nameType,dateTree=FALSE,labelType="b",labelOffset=.45,aspectRatio=1,pic="wiki",dotsConnectText=FALSE,picSize=1,picSaveDir,optPicWidth=200,picBorderWidth=10,picBorderCol="#363636",openDir=FALSE,xAxisPad=.2,xTitlePad=20,numXlabs=8,textScalar=1,xTitleScalar=1,phyloThickness=1.2,phyloCol="#363636",textCol="#363636",plotMar=c(t=.02,r=.5,b=.02,l=.02),clearCache=FALSE,quiet=TRUE,silent=FALSE,...){
   # Don't fully understand why this works, but we're passing all arguments into backend function
   mycall<-match.call()
   mycall[[1]]<-as.symbol("showPhylo_backend")
